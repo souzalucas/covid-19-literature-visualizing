4452416a2b9ed8a523e19c6e86ef1ce5d036a3ab
Lucas Souza <31828546+souzalucas@users.noreply.github.com>
2022-11-16 11:30:50 -0300

    Update capture_affiliation.py

--- capture_affiliation.py
+++ capture_affiliation.py
@@ -2,7 +2,7 @@
 # No arquivo de saída, a partir da linha com "------,-------" são os países que não puderam ser identificados
 # Usar no datawrapper apenas as linhas anteriores a essa
 
-# python3 capture_affiliation_v2.py dados/entrada_100.csv dados/normalized_countries_1.csv dados/countries_100.csv
+# python3 capture_affiliation.py sample_metadata.csv normalized_countries.csv out.csv
 
 import sys
 import csv
